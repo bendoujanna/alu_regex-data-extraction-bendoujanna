ca650b1560db4775f6731775eb6e1ec64cbd95be
bendoujanna <b.vitalina@alustudent.com>
2025-09-21 22:07:54 +0200

    Modified the functions

--- main.py
+++ main.py
@@ -2,18 +2,19 @@
 #regular expressions
 import re
 
-def validate_email(text):
+def extract_emails(text):
     pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-    return bool(re.match(pattern, text))
+    return re.findall(pattern, text)
 
-def validate_URL(text):
-    pattern = r'^(https?:\/\/[\w\-]+(\.[\w\-]+)+[^\s]*?)(?=\s|$)'
-    return bool(re.match(pattern, text))
+def extract_URLs(text):
+    pattern = r'^https:\/\/([\w-]+\.)+[\w-]+(\/[^\s]*)?$'
+    return re.findall(pattern, text)
 
-def validate_phoneNumber(text):
-    pattern = r'^(\(\d{3}\)|\d{3})[ .-]?\d{3}[ .-]?\d{4}$'
-    return bool(re.match(pattern, text))
+def extract_phoneNumbers(text):
+    pattern = r'^(\(\d{3}\)\s\d{3}-\d{4}|\d{3}[-.]\d{3}[-.]\d{4})$'
+    return re.findall(pattern, text)
 
-def validate_creditCard_number(text):
-    pattern = r'^\d{4}([ -]?)\d{4}\1\d{4}\1\d{4}$'
-    return bool(re.match(pattern, text))
+def extract_creditCard_numbers(text):
+    pattern = r'^\d{4}([ -])\d{4}\1\d{4}\1\d{4}$'
+    return re.findall(pattern, text)
+
