c610869dfe07b3c0bb3bbf01b9bb6fed242fb8f9
bendoujanna <b.vitalina@alustudent.com>
2025-09-22 11:33:08 +0200

    I fixed some patterns and made the program read the test files and extract the data types

--- main.py
+++ main.py
@@ -3,18 +3,35 @@
 import re
 
 def extract_emails(text):
-    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
     return re.findall(pattern, text)
 
 def extract_URLs(text):
-    pattern = r'^https:\/\/([\w-]+\.)+[\w-]+(\/[^\s]*)?$'
+    pattern = r'https:\/\/(?:[\w-]+\.)+[\w-]+(?:\/[^\s]*)?'
+
     return re.findall(pattern, text)
 
 def extract_phoneNumbers(text):
-    pattern = r'^(\(\d{3}\)\s\d{3}-\d{4}|\d{3}[-.]\d{3}[-.]\d{4})$'
+    pattern = r'(\(\d{3}\)\s\d{3}-\d{4}|\d{3}[-.]\d{3}[-.]\d{4})'
     return re.findall(pattern, text)
 
 def extract_creditCard_numbers(text):
-    pattern = r'^\d{4}([ -])\d{4}\1\d{4}\1\d{4}$'
+    pattern = r'\d{4}(?:[ -])\d{4}(?:[ -])\d{4}(?:[ -])\d{4}'
+
     return re.findall(pattern, text)
 
+files = ["test1.txt","test2.txt"]
+for filename in files:
+    print(f"\n...Extracting from {filename}...")
+    with open(filename, "r") as f:
+        data = f.read()
+
+    emails = extract_emails(data)
+    URLs = extract_URLs(data)
+    phoneNumbers = extract_phoneNumbers(data)
+    creditCardNumbers = extract_creditCard_numbers(data)
+
+    print("Emails found:", emails)
+    print("URLs found:", URLs)
+    print("PhoneNumbers found:", phoneNumbers)
+    print("CreditCards found:", creditCardNumbers)
